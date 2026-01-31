f1ee67f7a04a2a07bf8f9a069f7cd51786703ef9
yctseng0211 <yctseng@amd.com>
2026-01-31 00:39:58 -0600

    update the concurrency for gsmk benchmark

--- test/registered/amd/accuracy/mi35x/test_deepseek_v32_mtp_eval_mi35x.py
+++ test/registered/amd/accuracy/mi35x/test_deepseek_v32_mtp_eval_mi35x.py
@@ -98,8 +98,8 @@ class TestDeepseekV32TPMTP(CustomTestCase):
         args = SimpleNamespace(
             num_shots=20,
             data_path=None,
-            num_questions=1400,
-            parallel=1400,
+            num_questions=200,
+            parallel=64,
             max_new_tokens=512,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
